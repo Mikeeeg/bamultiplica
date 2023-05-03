07166bd62c7db8fd89f4c1e67b68c4e666a6ff06
MiqueasG <rubio.miqueas@gmail.com>
2023-05-02 22:47:13 -0300

    Actualización

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,53 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const menu = document.querySelector('.menu-items');
+/* Carrusel */
+const carousel = document.querySelector('#carouselExample');
+const prevButton = document.querySelector('.carousel-control-prev');
+const nextButton = document.querySelector('.carousel-control-next');
+const carouselInner = carousel.querySelector('.carousel-inner');
+const items = carouselInner.querySelectorAll('.carousel-item');
+const itemCount = items.length;
+let currentItem = 0;
 
-menuToggle.addEventListener('click', function() {
-  if (this.checked) {
-    menu.classList.add('open');
-  } else {
-    menu.classList.remove('open');
+function showCurrentItem() {
+  items.forEach((item, index) => {
+    if (index === currentItem) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+
+function movePrev() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = itemCount - 1;
   }
-});
+  showCurrentItem();
+}
+
+function moveNext() {
+  currentItem++;
+  if (currentItem >= itemCount) {
+    currentItem = 0;
+  }
+  showCurrentItem();
+}
+
+prevButton.addEventListener('click', movePrev);
+nextButton.addEventListener('click', moveNext);
+
+showCurrentItem();
+
+/* Pantalla de carga */
+window.onload = function() {
+  var pantallaCarga = document.getElementById("pantalla-carga");
+
+  setTimeout(function() {
+    pantallaCarga.style.display = "none";
+  }, 500); // 2000 milisegundos = 2 segundos
+};
+
+function noViene() {
+  alert("No 😭");
+}
+
